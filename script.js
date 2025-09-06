58ea722e5ed85699e92570df2fc897780043d0e5
Griffin1610 <PollyGS14@uww.edu>
2025-09-06 18:38:18 -0500

    updating css and fixing bug

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 //this function will first be called after answering question 1
 let currentQuestion = 1; //question 1 is already initialy in the html, our first change is to question 2
 let answerArray = [];
+let score = 0;
 function nextQuestion() {
     //add current button selection to answerArray, clear button selection
     const selected = document.querySelector('input[name="quiz-choice"]:checked');
     if(selected){
-        answerArray.push(selected.value);
+        answerArray[currentQuestion - 1] = selected.value;
         selected.checked = false;
         console.log(selected.value);
     }
@@ -16,7 +17,7 @@ function nextQuestion() {
     document.getElementById("image").src = `photos/question${currentQuestion}.jpg`;
 
     //once we reach question ten, calculate and display score
-    if (currentQuestion = 10) {
+    if (currentQuestion === 10) {
         calculateScore();
     }
 }
@@ -29,5 +30,12 @@ function previousQuestion() {
 }
 
 function calculateScore() {
-    let correctAnswer = ["","","","","","","","","",""];
+    let correctAnswers = ["a","b","c","a","d","c","c","d","b","b"];
+
+    for (let i = 0; i < correctAnswers.length; i++) {
+        if (answerArray[i] === correctAnswers[i]) {
+        score++;
+        }
+    }
+    alert(`Your score is ${score} out of 10.`);
 }
